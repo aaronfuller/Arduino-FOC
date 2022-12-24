e9637a84daef578b6c4fa7145fdeda003681289a
Aaron Fuller <aaron.s.fuller@gmail.com>
2022-12-24 02:37:38 -0600

    megatron sensor

--- src/sensors/megatron_cw360_4220.cpp
+++ src/sensors/megatron_cw360_4220.cpp
@@ -2,17 +2,14 @@
 #include "../common/time_utils.h"
 #include "megatron_cw360_4220.h"
 
-/** MagneticSensorAnalog(ADC_HandleTypeDef adc_handle)
- * @param adc_handle adc handle for sensor
+/** MagneticSensorAnalog(volatile uint16_t * adc_val)
+ * @param adc_val adc value for sensor
  */
-megatron_cw360_4220::megatron_cw360_4220(ADC_HandleTypeDef adc_handle) {
-    this->_adc_handle = adc_handle;
+megatron_cw360_4220::megatron_cw360_4220(volatile uint16_t * adc_val) {
+    this->_adc_val = adc_val;
 }
 
-
 void megatron_cw360_4220::init(){
-    HAL_ADC_Start_DMA(&(this->_adc_handle), (uint32_t*)(this->_adc_buf), ADC_BUF_LEN);
-    
     // Start max below probable minimum value for the max
     this->_adc_max = (uint16_t)(INIT_MAX_V / VREF) * (0xFFF);
     // Start min above probable max value for the min
@@ -27,7 +24,7 @@ void megatron_cw360_4220::init(){
 float megatron_cw360_4220::getSensorAngle(){
     // raw data from the sensor
     uint16_t buf_val, buf_val_less_offset;
-    buf_val = this->_adc_buf[0];
+    buf_val = *(this->_adc_val);
 
     if (buf_val > this->_adc_max) {
         this->_adc_max = buf_val;
--- src/sensors/megatron_cw360_4220.h
+++ src/sensors/megatron_cw360_4220.h
@@ -1,34 +1,29 @@
 #ifndef MEGATRON_CW360_4220
 #define MEGATRON_CW360_4220
-#define ADC_BUF_LEN 1
 #define VREF 3.3f
 #define INIT_MAX_V 2.55f
 #define INIT_MIN_V 0.60f
 
 #include "../common/base_classes/Sensor.h"
-#include "main.h"
 
 class megatron_cw360_4220: public Sensor{
     public:
         /**
          * Encoder class constructor
          */
-        megatron_cw360_4220(ADC_HandleTypeDef _adc_handle);
+        megatron_cw360_4220(volatile uint16_t * adc_val);
         void init();
         float getSensorAngle() override;
     
-        ADC_HandleTypeDef _adc_handle;
-
     private:
         /**
          * Function getting current angle register value
          * it uses angle_register variable
          */
-        uint16_t _adc_buf[ADC_BUF_LEN];
+        volatile uint16_t * _adc_val;
         uint16_t _adc_max;
         uint16_t _adc_min;
         uint16_t _adc_range;
-
 };
 
 
